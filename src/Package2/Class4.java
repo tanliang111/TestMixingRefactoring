6f66f7369f005a5ae3d64dcb8208cb456a358f7a
tanliang111 <tanliang9963@163.com>
2019-04-05 10:53:53 +0200

    MC+EM(new)

--- src/Package2/Class4.java
+++ src/Package2/Class4.java
@@ -1,5 +1,61 @@
 package Package2;
 
+import Package1.AbstractKeyPairProvider;
+import Package1.ArrayList;
+import Package1.FileInputStream;
+import Package1.FilePublicKeyProvider;
+import Package1.InputStreamReader;
+import Package1.JcaPEMKeyConverter;
+import Package1.KeyPair;
+import Package1.List;
+import Package1.Logger;
+import Package1.PEMKeyPair;
+import Package1.PublicKey;
+import Package1.SubjectPublicKeyInfo;
+
 public class Class4 {
 
+}
+class MoveClass extends AbstractKeyPairProvider {
+	  /** . */
+	  private static final Logger LOG = LoggerFactory.getLogger(FilePublicKeyProvider.class);
+	  /** . */
+	  private String[] files;
+	  FilePublicKeyProvider(String[] files) {
+	    this.files = files;
+	  }
+	  public Iterable<KeyPair> loadKeys() {
+	    if (!SecurityUtils.isBouncyCastleRegistered()) {
+	      throw new IllegalStateException("BouncyCastle must be registered as a JCE provider");
+	    }
+	    List<KeyPair> keys = new ArrayList<KeyPair>();
+	    for (String file : files) {
+	      try {
+	          Object o = KeyPairUtils.readKey(new InputStreamReader(new FileInputStream(file)));
+	          if (o instanceof KeyPair) {
+	            keys.add(new KeyPair(((KeyPair)o).getPublic(), null));
+	          } else if (o instanceof PublicKey) {
+	            keys.add(new KeyPair((PublicKey)o, null));
+	          } else if (o instanceof PEMKeyPair) {
+	        	  keys.add(convertPemKeyPair(keyPair));
+	          } else if (o instanceof SubjectPublicKeyInfo) {
+	            PEMKeyPair keyPair = new PEMKeyPair((SubjectPublicKeyInfo) o, null);
+	            keys.add(convertPemKeyPair(keyPair));
+	          } else if (o instanceof SubjectPublicKeyInfo) {
+	            PEMKeyPair keyPair = new PEMKeyPair((SubjectPublicKeyInfo) o, null);
+	            keys.add(convertPemKeyPair(keyPair));
+	          } else {
+	            throw new UnsupportedOperationException(String.format("Key type %s not supported.", o.getClass().getName()));
+	          }
+	      }
+	      catch (Exception e) {
+	        LOG.info("Unable to read key {}: {}", file, e);
+	      }
+	    }
+	    return keys;
+	  }
+	  private KeyPair convertPemKeyPair(PEMKeyPair pemKeyPair) throws PEMException {
+		    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+		    return new KeyPair(converter.getPublicKey(pemKeyPair.getPublicKeyInfo()), null);
+		  }
 }
