ee7eae8bf384bde09baff7e61757d497e5d274b7
tanliang111 <tanliang9963@163.com>
2019-04-05 13:58:33 +0200

    Extract Method+Inline Method(old)

--- src/Package1/Class1.java
+++ src/Package1/Class1.java
@@ -1,6 +1,34 @@
 package Package1;
 
 public class Class1 {
+	 public Iterable<KeyPair> loadKeys() {
+		    if (!SecurityUtils.isBouncyCastleRegistered()) {
+		      throw new IllegalStateException("BouncyCastle must be registered as a JCE provider");
+		    }
+		    List<KeyPair> keys = new ArrayList<KeyPair>();
+		    for (String file : files) {
+		      try {
+		          Object o = KeyPairUtils.readKey(new InputStreamReader(new FileInputStream(file)));
+		          if (o instanceof KeyPair) {
+		            keys.add(new KeyPair(((KeyPair)o).getPublic(), null));
+		          } else if (o instanceof PublicKey) {
+		            keys.add(new KeyPair((PublicKey)o, null));
+		          } else if (o instanceof PEMKeyPair) {
+		            PEMKeyPair keyPair = (PEMKeyPair)o;
+		            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+		            keys.add(new KeyPair(converter.getPublicKey(keyPair.getPublicKeyInfo()), null));
+		           
+		      }
+		      catch (Exception e) {
+		        LOG.info("Unable to read key {}: {}", file, e);
+		      }
+		    }
+		    return keys;
+		  }
+
+		    private boolean shouldShowTagToolbar() {
+		        return (getPostListType() == ReaderPostListType.TAG_FOLLOWED);
+		    }
 
 }
 
