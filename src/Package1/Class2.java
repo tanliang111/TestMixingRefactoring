f834d33a7978ccf35d930f017ce7b9d6b0cd8b52
tanliang111 <tanliang9963@163.com>
2019-04-05 15:44:26 +0200

    Extract Method+Push Down Method(old)

--- src/Package1/Class2.java
+++ src/Package1/Class2.java
@@ -2,12 +2,39 @@ package Package1;
 
 public class Class2 {
 
-	 public abc() {
-		    
-		    }
-	 private KeyPair convertPemKeyPair(PEMKeyPair pemKeyPair) throws PEMException {
-		    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-		    return new KeyPair(converter.getPublicKey(pemKeyPair.getPublicKeyInfo()), null);
-		  }
-	 
+	 private static final Logger LOG = LoggerFactory.getLogger(FilePublicKeyProvider.class);
+	  /** . */
+	  private String[] files;
+	  FilePublicKeyProvider(String[] files) {
+	    this.files = files;
+	  }
+	  public Iterable<KeyPair> loadKeys() {
+	    if (!SecurityUtils.isBouncyCastleRegistered()) {
+	      throw new IllegalStateException("BouncyCastle must be registered as a JCE provider");
+	    }
+	    List<KeyPair> keys = new ArrayList<KeyPair>();
+	    for (String file : files) {
+	      try {
+	          Object o = KeyPairUtils.readKey(new InputStreamReader(new FileInputStream(file)));
+	          if (o instanceof KeyPair) {
+	            keys.add(new KeyPair(((KeyPair)o).getPublic(), null));
+	          } else if (o instanceof PublicKey) {
+	            keys.add(new KeyPair((PublicKey)o, null));
+	          } else if (o instanceof PEMKeyPair) {
+	            PEMKeyPair keyPair = (PEMKeyPair)o;
+	            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+	            keys.add(new KeyPair(converter.getPublicKey(keyPair.getPublicKeyInfo()), null));
+	           
+	          }
+	      }
+	      catch (Exception e) {
+	        LOG.info("Unable to read key {}: {}", file, e);
+	      }
+	    }
+	    return keys;
+	  }
+
+	  
+
+	}
 }
--- src/Package1/Class3.java
+++ src/Package1/Class3.java
@@ -2,39 +2,9 @@ package Package1;
 
 public class Class3 extends Class2 {
 
-	 private static final Logger LOG = LoggerFactory.getLogger(FilePublicKeyProvider.class);
-	  /** . */
-	  private String[] files;
-	  FilePublicKeyProvider(String[] files) {
-	    this.files = files;
-	  }
-	  public Iterable<KeyPair> loadKeys() {
-	    if (!SecurityUtils.isBouncyCastleRegistered()) {
-	      throw new IllegalStateException("BouncyCastle must be registered as a JCE provider");
+	public abc() {
+	    
 	    }
-	    List<KeyPair> keys = new ArrayList<KeyPair>();
-	    for (String file : files) {
-	      try {
-	          Object o = KeyPairUtils.readKey(new InputStreamReader(new FileInputStream(file)));
-	          if (o instanceof KeyPair) {
-	            keys.add(new KeyPair(((KeyPair)o).getPublic(), null));
-	          } else if (o instanceof PublicKey) {
-	            keys.add(new KeyPair((PublicKey)o, null));
-	          } else if (o instanceof PEMKeyPair) {
-	            PEMKeyPair keyPair = (PEMKeyPair)o;
-	            
-	          }
-	      }
-	      catch (Exception e) {
-	        LOG.info("Unable to read key {}: {}", file, e);
-	      }
-	    }
-	    return keys;
-	  }
-
-	 
-	  
-
-	}
+	
 	
 }
