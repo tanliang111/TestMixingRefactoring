38ceeb45d0488563787f595fc779b63bb9b32367
tanliang111 <tanliang9963@163.com>
2019-04-10 15:21:59 +0200

    Inline Method+Move Method(new1)

--- src/Package3/Class12.java
+++ src/Package3/Class12.java
@@ -4,5 +4,9 @@ public class Class12 {
 public abb() {
 		
 	}
-
+public bcd() {
+	if (paymentInfoPlugin == null) {
+        throw new IllegalStateException("Payment plugin returned a null result");
+    }
+    }
 }
--- src/Package4/Class11.java
+++ src/Package4/Class11.java
@@ -6,13 +6,7 @@ public cde() {
 		
 	}
 	
-public bcd() {
-	 
-    }
 
-private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-    if (paymentInfoPlugin == null) {
-        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-    }
-}
+
+
 }
