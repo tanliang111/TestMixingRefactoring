eb6374e8d7b281cef2de443ee67f58248221bf8f
tanliang111 <tanliang9963@163.com>
2019-04-10 14:44:51 +0200

    Inline Method+Pull Up Method(new)

--- src/Package4/Class10.java
+++ src/Package4/Class10.java
@@ -4,5 +4,11 @@ public class Class10 {
 	
 	public abc() {}
    
+	public bcd() {
+		if (paymentInfoPlugin == null) {
+            throw new IllegalStateException("Payment plugin returned a null result");
+        }
+	}
+	
 	
 }
