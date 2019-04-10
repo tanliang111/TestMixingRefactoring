092027cf98281e16d2deb2f3ed45e2bced703827
tanliang111 <tanliang9963@163.com>
2019-04-10 15:47:19 +0200

    Inline Method+Move Class(new)

---
+++ src/Package3/Class12.java
@@ -0,0 +1,13 @@
+package Package3;
+
+public class Class12 {
+public abb() {
+		
+	}
+public bcd() {
+	if (paymentInfoPlugin == null) {
+        throw new IllegalStateException("Payment plugin returned a null result");
+    }
+    }
+
+}
