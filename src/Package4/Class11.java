aaba1d5663e4da2f7c59d22e32f4eea77cd6e3c8
tanliang111 <tanliang9963@163.com>
2019-04-10 15:03:58 +0200

    Inline Method+Move Method(new)

--- src/Package4/Class11.java
+++ src/Package4/Class11.java
@@ -6,14 +6,6 @@ public cde() {
 		
 	}
 	
-public bcd() {
-	
-}
-private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-    if (paymentInfoPlugin == null) {
-        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-    }
-}
 
 
 }
