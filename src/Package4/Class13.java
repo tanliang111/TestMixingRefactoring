868c143366410bd87cf4a4e2e5fb7fa23eb84da5
tanliang111 <tanliang9963@163.com>
2019-04-11 10:05:50 +0200

    Inline Method+Move Class(old3)

---
+++ src/Package4/Class13.java
@@ -0,0 +1,15 @@
+package Package4;
+
+public class Class13 {
+
+}
+public class Inline {
+public abt() {
+		
+	}
+private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+    if (paymentInfoPlugin == null) {
+        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+    }
+}
+}
