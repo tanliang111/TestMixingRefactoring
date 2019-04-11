bb03b0ace2cee641c1faa50e32a2f9d6c8bb39a8
tanliang111 <tanliang9963@163.com>
2019-04-11 14:57:32 +0200

    Pull Up Method+Extract SuperClass(new)

--- src/Package4/Class10.java
+++ src/Package4/Class10.java
@@ -6,9 +6,5 @@ public class Class10 extends Class14{
 	
 	public abc() {}
 	
-	private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-	    if (paymentInfoPlugin == null) {
-	        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-	    }
-	}
+	
 }
