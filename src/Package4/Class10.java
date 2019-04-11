cf12036a4170875f95f113ac2cef510d4a2edfbc
tanliang111 <tanliang9963@163.com>
2019-04-11 14:29:10 +0200

    Pull Up Method+Move Class(old2)

--- src/Package4/Class10.java
+++ src/Package4/Class10.java
@@ -1,14 +1,19 @@
 package Package4;
 
-import Package3.Class14;
-
 public class Class10 extends Class14{
 	
 	public abc() {}
 	
-	 private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-	        if (paymentInfoPlugin == null) {
-	            throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-	        }
-	 }
+	 
+}
+public class Class15 extends Class14{
+	
+	public acc() {}
+	
+	private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+        if (paymentInfoPlugin == null) {
+            throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+        }
+ }
+}
 }
