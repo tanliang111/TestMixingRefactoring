e022bdc61009f7c74f7955e49bb81015a4c7b669
tanliang111 <tanliang9963@163.com>
2019-04-11 10:42:29 +0200

    Inline Method+Extract SuperClass(new)

--- src/Package4/Class10.java
+++ src/Package4/Class10.java
@@ -1,15 +1,9 @@
 package Package4;
 
-public class Class10 {
+public class Class10 extends Class14{
 	
 	public abc() {}
 	
-	public abi() {}
 	
-	private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-        if (paymentInfoPlugin == null) {
-            throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-        }
-	}
 
 }
---
+++ src/Package4/Class14.java
@@ -0,0 +1,9 @@
+package Package4;
+
+public abstract class Class14 {
+public abi() { if (paymentInfoPlugin == null) {
+    throw new IllegalStateException("Payment plugin returned a null result");
+}}
+	
+	
+}
