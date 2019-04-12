21e4e89731a4bcf4e6b8d79430d470d4c7bc65fc
tanliang111 <tanliang9963@163.com>
2019-04-12 10:18:29 +0200

    Push Down Method+Move Class(old)

--- src/Package3/Class12.java
+++ src/Package3/Class12.java
@@ -7,11 +7,7 @@ public class Class12 {
 public abb() {
 		
 	}
-private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-    if (paymentInfoPlugin == null) {
-        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-    }
-}
+
 }
 public class Inline {
 public abt() {
