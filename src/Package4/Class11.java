7c888e466703dfde277d016f5ca8b6ce9bce7117
tanliang111 <tanliang9963@163.com>
2019-04-12 11:35:11 +0200

    Move Method+Move Class(new)

---
+++ src/Package4/Class11.java
@@ -0,0 +1,19 @@
+package Package4;
+
+import Package4.PaymentApiException;
+import Package4.PaymentTransactionInfoPlugin;
+
+public class Class11 extends Class10 {
+
+public cde() {
+		
+	}
+	
+
+private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+    if (paymentInfoPlugin == null) {
+        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+    }
+}
+
+}
