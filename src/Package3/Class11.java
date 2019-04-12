82a670cc34e5cdd4fdf5b1f30f4a505a87d73622
tanliang111 <tanliang9963@163.com>
2019-04-12 12:06:31 +0200

    Move Method+Rename Package(old)

--- src/Package3/Class11.java
+++ src/Package3/Class11.java
@@ -9,5 +9,9 @@ public class Class11 extends Class10 {
 public cde() {
 		
 	}
-
+private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+    if (paymentInfoPlugin == null) {
+        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+    }
+}
 }
--- src/Package4/Class16.java
+++ src/Package4/Class16.java
@@ -6,10 +6,6 @@ public class Class16 {
 		super();
 	}
 
-	private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-	    if (paymentInfoPlugin == null) {
-	        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-	    }
-	}
+
 
 }
