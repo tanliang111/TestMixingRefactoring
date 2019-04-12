8eee45f64d802b6774e630e63e07c3d0abf873e5
tanliang111 <tanliang9963@163.com>
2019-04-12 12:01:47 +0200

    Move Method+ Extract SuperClass(old1)

--- src/Package4/Class14.java
+++ src/Package4/Class14.java
@@ -3,6 +3,7 @@ package Package4;
 import Package3.PaymentApiException;
 import Package3.PaymentTransactionInfoPlugin;
 
-public class Class14 extends Class16 {
+public class Class14 {
 public abi() { }
+
 }
