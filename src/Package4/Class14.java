af5e624b87bc1957d48229d843a2b3a8e95d6b1f
tanliang111 <tanliang9963@163.com>
2019-04-12 12:04:51 +0200

    Move Method+ Extract SuperClass(new)

--- src/Package4/Class14.java
+++ src/Package4/Class14.java
@@ -3,7 +3,6 @@ package Package4;
 import Package3.PaymentApiException;
 import Package3.PaymentTransactionInfoPlugin;
 
-public class Class14 {
+public class Class14 extends Class16 {
 public abi() { }
-
 }
---
+++ src/Package4/Class16.java
@@ -0,0 +1,15 @@
+package Package4;
+
+public class Class16 {
+
+	public Class16() {
+		super();
+	}
+
+	private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+	    if (paymentInfoPlugin == null) {
+	        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+	    }
+	}
+
+}
