63968c857b867af973eb4c72549bc740125cee33
tanliang111 <tanliang9963@163.com>
2019-04-12 11:44:38 +0200

    Move Method+Move Class(new)

---
+++ src/Package3/Class11.java
@@ -0,0 +1,19 @@
+package Package3;
+
+import Package4.Class14;
+import Package4.PaymentApiException;
+import Package4.PaymentTransactionInfoPlugin;
+
+public class Class11 extends Class10 {
+
+public cde() {
+		
+	}
+	
+}
+public class Class17 {
+	
+	public anm() {}
+	
+	
+}
