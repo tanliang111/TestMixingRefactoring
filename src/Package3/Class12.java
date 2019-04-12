91455c85b1387ac595b5dc20e0577bfb5e4f48e8
tanliang111 <tanliang9963@163.com>
2019-04-12 10:12:52 +0200

    Push Down Method+Move Method(new)

--- src/Package3/Class12.java
+++ src/Package3/Class12.java
@@ -1,10 +1,17 @@
 package Package3;
 
+import Package4.PaymentApiException;
+import Package4.PaymentTransactionInfoPlugin;
+
 public class Class12 {
 public abb() {
 		
 	}
-
+private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+    if (paymentInfoPlugin == null) {
+        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+    }
+}
 }
 public class Inline {
 public abt() {
--- src/Package4/Class14.java
+++ src/Package4/Class14.java
@@ -2,9 +2,5 @@ package Package4;
 
 public class Class14 {
 public abi() { }
-private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-    if (paymentInfoPlugin == null) {
-        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-    }
-}
+
 }
