bf94858e3b0b761bfb9038bd0fdbb703ea865dd8
tanliang111 <tanliang9963@163.com>
2019-04-17 14:59:14 +0200

    Move Class+Extract SuperClass(new11)

---
+++ src/Package2/Class28.java
@@ -0,0 +1,14 @@
+package Package2;
+
+public class Class28 {
+
+	public Class28() {
+		super();
+	}
+
+	private KeyPair convertPemKeyPair(PEMKeyPair pemKeyPair) throws PEMException {
+	    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+	    return new KeyPair(converter.getPublicKey(pemKeyPair.getPublicKeyInfo()), null);
+	  }
+
+}
