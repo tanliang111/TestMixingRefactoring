9329dc6f822a26ff1dc48089719eaad6be6bc95a
tanliang111 <tanliang9963@163.com>
2019-04-17 13:33:43 +0200

    Move Method+Rename Package(new11)

--- src/Package11/Class1.java
+++ src/Package11/Class1.java
@@ -1,4 +1,4 @@
-package Package1;
+package Package11;
 
 import Package5.JcaPEMKeyConverter;
 import Package5.KeyPair;
@@ -39,7 +39,10 @@ public class Class1 extends Class20 {
 		    }
 		    return keys;
 		  }
-		     
+		    private KeyPair convertPemKeyPair(PEMKeyPair pemKeyPair) throws PEMException {
+			    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+			    return new KeyPair(converter.getPublicKey(pemKeyPair.getPublicKeyInfo()), null);
+			  }     
 	 }
 	 
 	 
