eef27ee8acc6caedacc9ad89cd219c66c01db504
tanliang111 <tanliang9963@163.com>
2019-04-17 10:57:44 +0200

    Move Method+Move Class(old111)

--- src/Package4/Class13.java
+++ src/Package4/Class13.java
@@ -1,4 +1,4 @@
-package Package3;
+package Package4;
 
 import Package5.JcaPEMKeyConverter;
 import Package5.KeyPair;
@@ -11,8 +11,5 @@ public class Class13 {
             throw new IllegalStateException("Payment plugin returned a null result");
         }
 	}
-	private KeyPair convertPemKeyPair(PEMKeyPair pemKeyPair) throws PEMException {
-	    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-	    return new KeyPair(converter.getPublicKey(pemKeyPair.getPublicKeyInfo()), null);
-	  }  
+	
 	}
