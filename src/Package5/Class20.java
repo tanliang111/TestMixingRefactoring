fdc2245139f80681647c9e0d67c0f3da57964e65
tanliang111 <tanliang9963@163.com>
2019-04-17 13:28:13 +0200

    Move Method+Rename Package(old1)

--- src/Package5/Class20.java
+++ src/Package5/Class20.java
@@ -6,52 +6,13 @@ import Package4.PEMException;
 import Package4.PEMKeyPair;
 
 public class Class20 {
-	private static final Logger LOG = LoggerFactory.getLogger(FilePublicKeyProvider.class);
-	  /** . */
-	  private String[] files;
-	  FilePublicKeyProvider(String[] files) {
-	    this.files = files;
-	  }
-	  public Iterable<KeyPair> loadKeys() {
-	    if (!SecurityUtils.isBouncyCastleRegistered()) {
-	      throw new IllegalStateException("BouncyCastle must be registered as a JCE provider");
+	private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+	    if (paymentInfoPlugin == null) {
+	        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
 	    }
-	    List<KeyPair> keys = new ArrayList<KeyPair>();
-	    for (String file : files) {
-	      try {
-	          Object o = KeyPairUtils.readKey(new InputStreamReader(new FileInputStream(file)));
-	          if (o instanceof KeyPair) {
-	            keys.add(new KeyPair(((KeyPair)o).getPublic(), null));
-	          } else if (o instanceof PublicKey) {
-	            keys.add(new KeyPair((PublicKey)o, null));
-	          } else if (o instanceof PEMKeyPair) {
-	        	  keys.add(convertPemKeyPair(keyPair));
-	          } else if (o instanceof SubjectPublicKeyInfo) {
-	            PEMKeyPair keyPair = new PEMKeyPair((SubjectPublicKeyInfo) o, null);
-	            
-	            keys.add(convertPemKeyPair(keyPair));
-	          } else if (o instanceof SubjectPublicKeyInfo) {
-	            PEMKeyPair keyPair = new PEMKeyPair((SubjectPublicKeyInfo) o, null);
-	           
-	            keys.add(convertPemKeyPair(keyPair));
-	          } else {
-	            throw new UnsupportedOperationException(String.format("Key type %s not supported.", o.getClass().getName()));
-
-	          }
-	      }
-	      catch (Exception e) {
-	        LOG.info("Unable to read key {}: {}", file, e);
-	      }
-	    }
-	    return keys;
-	  }	
+	    private KeyPair convertPemKeyPair(PEMKeyPair pemKeyPair) throws PEMException {
+		    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+		    return new KeyPair(converter.getPublicKey(pemKeyPair.getPublicKeyInfo()), null);
+		  }   
 	
 }
-	class Class25 {
-		private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-		    if (paymentInfoPlugin == null) {
-		        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-		    }
-		   
-	}
-	
