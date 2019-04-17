e18c4e638947479af032593dc73a75ae2827d7e4
tanliang111 <tanliang9963@163.com>
2019-04-17 10:49:19 +0200

    Move Method+Move Class(old11)

--- src/Package5/Class21.java
+++ src/Package5/Class21.java
@@ -1,11 +1,6 @@
-package Package55;
+package Package5;
 
-import Package4.JcaPEMKeyConverter;
-import Package4.KeyPair;
-import Package4.PEMException;
-import Package4.PEMKeyPair;
-
-public class Class22 extends Class20 {
+public class Class21 {
 	private static final Logger LOG = LoggerFactory.getLogger(FilePublicKeyProvider.class);
 	  /** . */
 	  private String[] files;
@@ -28,7 +23,8 @@ public class Class22 extends Class20 {
 	        	  keys.add(convertPemKeyPair(keyPair));
 	          } else if (o instanceof SubjectPublicKeyInfo) {
 	            PEMKeyPair keyPair = new PEMKeyPair((SubjectPublicKeyInfo) o, null);
-	            
+	            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
+	            keys.add(new KeyPair(converter.getPublicKey(keyPair.getPublicKeyInfo()), null));
 	            keys.add(convertPemKeyPair(keyPair));
 	          } else if (o instanceof SubjectPublicKeyInfo) {
 	            PEMKeyPair keyPair = new PEMKeyPair((SubjectPublicKeyInfo) o, null);
@@ -45,11 +41,10 @@ public class Class22 extends Class20 {
 	    }
 	    return keys;
 	  }
-	private KeyPair convertPemKeyPair(PEMKeyPair pemKeyPair) throws PEMException {
-	    JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-	    return new KeyPair(converter.getPublicKey(pemKeyPair.getPublicKeyInfo()), null);
-	  }
 
 	 
-}
+
+
+
+} 
 }
