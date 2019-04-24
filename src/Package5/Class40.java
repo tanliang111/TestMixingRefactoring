dbd091a8cf45f71899a9d28a7b56a3543db2b190
tanliang111 <tanliang9963@163.com>
2019-04-24 15:14:41 +0200

    Pull Up Method+Move Method(new11)

--- src/Package5/Class40.java
+++ src/Package5/Class40.java
@@ -1,10 +1,16 @@
 package Package5;
 
 import Package6.PaymentApiException;
+import Package6.PaymentTransactionInfoPlugin;
 
 public class Class40 {
 	public void abb() {
 
 }
-	
+	private void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+	    if (paymentInfoPlugin == null) {
+	        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+	    }
+	}
+
 }
--- src/Package6/Class38.java
+++ src/Package6/Class38.java
@@ -7,5 +7,4 @@ public class Class38 {
 
 
 
-
 }
