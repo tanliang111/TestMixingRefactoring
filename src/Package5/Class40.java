c9093a0f60d121c01d4a81d652cba200bb9518af
tanliang111 <tanliang9963@163.com>
2019-04-24 14:44:37 +0200

    Inline Method+Move Method(new11)

--- src/Package5/Class40.java
+++ src/Package5/Class40.java
@@ -1,8 +1,14 @@
 package Package5;
 
+import Package6.PaymentApiException;
+
 public class Class40 {
 	public void abb() {
 
 	}
-
+	public void bcd() {
+		if (paymentInfoPlugin == null) {
+	        throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+	    }
+	    }
 }
