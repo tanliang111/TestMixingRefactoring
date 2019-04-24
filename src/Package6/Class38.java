6c8f24493d79fce0a24062de25b07415b26c7f52
tanliang111 <tanliang9963@163.com>
2019-04-24 16:32:33 +0200

    Move Class+Extract SuperClass(old22222)

--- src/Package6/Class38.java
+++ src/Package6/Class38.java
@@ -1,11 +1,23 @@
 package Package6;
 
-import Package5.PaymentApiException;
-import Package5.PaymentTransactionInfoPlugin;
 
-public class Class38 extends Class39 {
-	public void cde() {
-
+public class Class38 {
+	
+	private String name;
+	private Integer age;
+	
+	public Student (String name,Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Integer getAge() {
+		return this.age;
+	}
+	
+	public static void main(String[] args) {
+		Bro bro = new Bro(11);
+		System.out.println(bro.getAge());
 	}
 
 }
