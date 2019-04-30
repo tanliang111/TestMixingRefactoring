6a14ea55ec84bad42a5e53ab327c1704d77ed9cb
tanliang111 <tanliang9963@163.com>
2019-04-30 16:24:01 +0200

    Extract Interface+Rename Package(old11111)

--- src/Package5/Interface.java
+++ src/Package5/Interface.java
@@ -2,8 +2,9 @@ package Package5;
 
 import java.util.Set;
 
-public interface Interface extends InterfaceAAA {
+public interface Interface {
 	
 		    void resolveNow();
 		    Set<Interface> getArtifacts(long id);
+		    Set<Interface> getArtifacts();
 		}
---
+++ src/Package6/Interface123.java
@@ -0,0 +1,5 @@
+package Package6;
+
+public interface Interface123 {
+
+}
