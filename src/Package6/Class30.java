f21431eab32d298cd2b748d3ae4f5e722c28998f
tanliang111 <tanliang9963@163.com>
2019-05-06 13:51:15 +0200

    Move Class+Extract Interface(old1)

--- src/Package6/Class30.java
+++ src/Package6/Class30.java
@@ -51,6 +51,9 @@ public class Class30 {
 	 
 }
 class Class31 {
+	
+	Iterable<KeyPair> loadKeys();
+	
 	public void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
         if (paymentInfoPlugin == null) {
             throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
