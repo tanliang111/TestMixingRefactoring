2ff2dbd9581388574c6bc88fed3bdd29c4f6a453
tanliang111 <tanliang9963@163.com>
2019-05-06 10:55:12 +0200

    Move Attribute+Extract SuperClass(new)

--- src/Package7/Class60.java
+++ src/Package7/Class60.java
@@ -3,16 +3,10 @@ package Package7;
 import Package6.PaymentApiException;
 import Package6.PaymentTransactionInfoPlugin;
 
-public class Class60 {
+public class Class60 extends Class61 {
 	private static final String NEW_PASSWORD_PARAMETER = "new-password";
 	
 	private static final Field NEW_PASSWORD = new Field(NEW_PASSWORD_PARAMETER, "New password", Field.Type.PASSWORD);
 	
 	private static final Field CONFIRM_NEW_PASSWORD = new Field(CONFIRM_NEW_PASSWORD_PARAMETER, "Confirm new password", Field.Type.PASSWORD);
-	
-	public void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
-        if (paymentInfoPlugin == null) {
-            throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
-        }
-}
 }
---
+++ src/Package7/Class61.java
@@ -0,0 +1,15 @@
+package Package7;
+
+public class Class61 {
+
+	public Class61() {
+		super();
+	}
+
+	public void sanityOnPaymentInfoPlugin(final PaymentTransactionInfoPlugin paymentInfoPlugin) throws PaymentApiException {
+	        if (paymentInfoPlugin == null) {
+	            throw new PaymentApiException(ErrorCode.PAYMENT_PLUGIN_EXCEPTION, "Payment plugin returned a null result");
+	        }
+	}
+
+}
